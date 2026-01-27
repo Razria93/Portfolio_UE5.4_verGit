5675f0cf1e08ad240304a99b1d599b97cac811b4
Razria <dsyeom931008@gmail.com>
2026-01-27 14:46:09 +0900

    ✨ feat(ai-behaviortree-core): add ACAIController with Blackboard / BehaviorTree initialization on possess (#26)

--- Source/Portfolio/Controller/CAIController.cpp
+++ Source/Portfolio/Controller/CAIController.cpp
@@ -1,1 +1,46 @@
 #include "Controller/CAIController.h"
+#include "ProjectGlobal.h"
+
+#include "Perception/AIPerceptionComponent.h"
+
+ACAIController::ACAIController()
+{
+	// Init AIPerceptionComp
+	AIPerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+	check(AIPerceptionComp);
+}
+
+void ACAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	if (!IsValid(InPawn)) return;
+
+	ControlledPawn_Cached = InPawn;
+	
+	InitializeBlackboardAndTree();
+}
+
+void ACAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+}
+
+void ACAIController::InitializeBlackboardAndTree()
+{
+	if (BlackboardAsset)
+	{
+		UBlackboardComponent* blackboardComp = GetBlackboardComponent();
+		UseBlackboard(BlackboardAsset, blackboardComp);
+	}
+
+	if (BehaviorTreeAsset)
+	{
+		RunBehaviorTree(BehaviorTreeAsset);
+	}
+}
--- Source/Portfolio/Controller/CAIController.h
+++ Source/Portfolio/Controller/CAIController.h
@@ -9,4 +9,31 @@ class PORTFOLIO_API ACAIController : public AAIController
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	class UBlackboardData* BlackboardAsset;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UBehaviorTree* BehaviorTreeAsset;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	class UAIPerceptionComponent* AIPerceptionComp;
+
+protected:
+	UPROPERTY(Transient)
+	class APawn* ControlledPawn_Cached;
+
+public:
+	ACAIController();
+
+protected:
+	void BeginPlay() override;
+
+protected:
+	void OnPossess(APawn* InPawn) override;
+	void OnUnPossess() override;
+
+private:
+	void InitializeBlackboardAndTree();
 };
