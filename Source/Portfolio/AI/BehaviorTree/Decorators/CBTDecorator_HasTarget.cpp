add07cc5d8c9caec954d613b1cc766ca5610e80b
Razria <dsyeom931008@gmail.com>
2026-02-10 22:36:25 +0900

    ✨ feat(ai-behaviortree-core): implement CBTDecorator_HasTarget (#26)

---
+++ Source/Portfolio/AI/BehaviorTree/Decorators/CBTDecorator_HasTarget.cpp
@@ -0,0 +1,20 @@
+#include "AI/BehaviorTree/Decorators/CBTDecorator_HasTarget.h"
+#include "ProjectGlobal.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+#include "AI/BlackBoard/CAIKeys.h"
+
+UCBTDecorator_HasTarget::UCBTDecorator_HasTarget()
+{
+	NodeName = "Has Target";
+	TargetActorKey.SelectedKeyName = CAIKeys::TargetActor;
+}
+
+bool UCBTDecorator_HasTarget::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const UBlackboardComponent* blackboard = OwnerComp.GetBlackboardComponent();
+	if (!blackboard) return false;
+
+	return blackboard->GetValueAsObject(TargetActorKey.SelectedKeyName) != nullptr;
+}
---
+++ Source/Portfolio/AI/BehaviorTree/Decorators/CBTDecorator_HasTarget.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "CBTDecorator_HasTarget.generated.h"
+
+UCLASS()
+class PORTFOLIO_API UCBTDecorator_HasTarget : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UCBTDecorator_HasTarget();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetActorKey; // Cached KeyName
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
