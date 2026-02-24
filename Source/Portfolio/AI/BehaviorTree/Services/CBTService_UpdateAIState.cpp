b034d20153d72630e7ea4126e7ba35b8fefc7543
Razria <dsyeom931008@gmail.com>
2026-02-24 15:04:05 +0900

    ✨ feat(ai-behaviortree-core): implement CBTService_UpdateAIState and delete CBTTask_SetAIState (#26)

--- Source/Portfolio/AI/BehaviorTree/Services/CBTService_UpdateAIState.cpp
+++ Source/Portfolio/AI/BehaviorTree/Services/CBTService_UpdateAIState.cpp
@@ -1,2 +1,95 @@
 #include "AI/BehaviorTree/Services/CBTService_UpdateAIState.h"
+#include "ProjectGlobal.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+
+#include "AI/BlackBoard/CAIKey.h"
+
+UCBTService_UpdateAIState::UCBTService_UpdateAIState()
+{
+	NodeName = "Update AIState";
+	bNotifyTick = true;
+
+	Interval = 0.1f;
+	RandomDeviation = 0.02f;
+}
+
+void UCBTService_UpdateAIState::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	UWorld* world = GetWorld();
+	if (!IsValid(world)) return;
+
+	UBlackboardComponent* blackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!IsValid(blackboardComp)) return;
+
+	const float currentTime = world->GetTimeSeconds();
+
+	const EAIStateType nextAIStateType = DecideNextAIStateType(blackboardComp, currentTime);
+
+	ChangeAIStateType(blackboardComp, nextAIStateType);
+}
+
+EAIStateType UCBTService_UpdateAIState::DecideNextAIStateType(UBlackboardComponent* InBlackboard, float InCurrentTime) const
+{
+	// -------------------------------------------------
+	// 1. Absolute States
+	// -------------------------------------------------
+	if (InBlackboard->GetValueAsBool(CAIKey::Lifecycle::bIsDead))
+		return EAIStateType::Dead;
+
+	if (InBlackboard->GetValueAsBool(CAIKey::Reaction::bIsHitReacting))
+		return EAIStateType::HitReact;
+
+	// -------------------------------------------------------------------------
+	// 2.  Context
+	// -------------------------------------------------------------------------
+	AActor* target = Cast<AActor>(InBlackboard->GetValueAsObject(CAIKey::Targeting::TargetActor));
+
+	const bool bHasTarget = IsValid(target);
+	const bool bHasLOS = InBlackboard->GetValueAsBool(CAIKey::Perception::bHasLOS);
+	const bool bHasMemory = InBlackboard->GetValueAsBool(CAIKey::Perception::bHasMemory);
+
+	const bool bInRange = InBlackboard->GetValueAsBool(CAIKey::Combat::bInRange);
+	const bool bCanAttack = InBlackboard->GetValueAsBool(CAIKey::Combat::bCanAttack);
+
+	// -------------------------------------------------------------------------
+	// 3) Decide Next AIStateType
+	// -------------------------------------------------------------------------
+	// 3-1. Invalid Target -> Idle
+	if (!bHasTarget || !bHasMemory)
+	{
+		return EAIStateType::Idle;
+	}
+
+	// 3-2. Valid target But Invalid LOS
+	if (!bHasLOS)
+	{
+		return EAIStateType::Investigate;
+	}
+
+	// 3-3. Valid Target and LOS But Out of Range
+	if (!bInRange)
+	{
+		return EAIStateType::Chase;
+	}
+
+	// 3-4. in Range But attack disable
+	if (!bCanAttack)
+	{
+		return EAIStateType::Alert;
+	}
+
+	// 3-5. in Range and Attackable
+	return EAIStateType::Combat;
+}
+
+bool UCBTService_UpdateAIState::ChangeAIStateType(UBlackboardComponent* InBlackboard, EAIStateType InNextAIStateType) const
+{
+	const uint8 currentAIStateType = static_cast<uint8>(InBlackboard->GetValueAsEnum(CAIKey::State::AIStateType));
+	const uint8 nextAIStateType = static_cast<uint8>(InNextAIStateType);
+
+	if (currentAIStateType == nextAIStateType) return false;
+
+	InBlackboard->SetValueAsEnum(CAIKey::State::AIStateType, nextAIStateType);
+	return true;
+}
--- Source/Portfolio/AI/BehaviorTree/Services/CBTService_UpdateAIState.h
+++ Source/Portfolio/AI/BehaviorTree/Services/CBTService_UpdateAIState.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
+#include "Type/CAIStateStructure.h"
 #include "CBTService_UpdateAIState.generated.h"
 
 UCLASS()
@@ -9,4 +10,13 @@ class PORTFOLIO_API UCBTService_UpdateAIState : public UBTService
 {
 	GENERATED_BODY()
 	
+public:
+	UCBTService_UpdateAIState();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+	EAIStateType DecideNextAIStateType(UBlackboardComponent* InBlackboard, float InCurrentTime) const;
+	bool ChangeAIStateType(UBlackboardComponent* InBlackboard, EAIStateType InNextAIStateType) const;
 };
