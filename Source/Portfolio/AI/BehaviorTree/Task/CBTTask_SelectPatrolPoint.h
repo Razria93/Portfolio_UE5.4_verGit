d1797f399ee450d49f8520d92a18a1dec8983e92
Razria <dsyeom931008@gmail.com>
2026-03-06 13:05:17 +0900

    🎨 style(ai-behaviortree-core): add comment (#26)

--- Source/Portfolio/AI/BehaviorTree/Task/CBTTask_SelectPatrolPoint.h
+++ Source/Portfolio/AI/BehaviorTree/Task/CBTTask_SelectPatrolPoint.h
@@ -2,14 +2,20 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
-
 #include "CBTTask_SelectPatrolPoint.generated.h"
 
 UCLASS()
 class PORTFOLIO_API UCBTTask_SelectPatrolPoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
+
+	/********************************************
+	*
+	* [NOTE] Regacy Task
+	* - replaced UCBTService_UpdatePatrolContext
+	* 
+	********************************************/
+
 public:
 	UCBTTask_SelectPatrolPoint();
 
