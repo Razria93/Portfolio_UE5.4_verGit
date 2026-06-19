053171b09b1ce91f79ad76ff7da1b95e3d04e305
Razria <dsyeom931008@gmail.com>
2026-06-19 21:59:19 +0900

    feat(combat): trace parry reaction feedback matching

--- Source/Portfolio/Component/CReactionFeedbackComponent.h
+++ Source/Portfolio/Component/CReactionFeedbackComponent.h
@@ -37,6 +37,7 @@ public:
 
 private:
 	bool CanPlayReactionFeedback(const FReactionFeedbackRequest& InReactionFeedbackRequest) const;
+	bool IsParryFeedbackRequest(const FReactionFeedbackRequest& InReactionFeedbackRequest) const;
 
 private:
 	bool TryCalculateMatchScore(const FReactionFeedbackKey& InDataKey, EReactionFeedbackTiming InDataTiming, FName InDataTriggerKey, const FReactionFeedbackRequest& InReactionFeedbackRequest, int32& OutScore) const;
