46525ee3b5bdefce81eac0bf4cc07623d9ac399e
Razria <dsyeom931008@gmail.com>
2026-08-06 05:02:09 +0900

    chore(debug-overlay): reorder internals API declarations by call flow

--- Source/Portfolio/Core/Debug/FDebugOverlaySnapshotStoreInternals.h
+++ Source/Portfolio/Core/Debug/FDebugOverlaySnapshotStoreInternals.h
@@ -44,10 +44,10 @@ namespace SnapshotStoreConfig
 {
 	bool IsEnabled();
 	bool IsCollecting();
-	bool ShouldHideNoiseEvents();
-	bool ShouldHideCollisionWindowEvents();
 	int32 GetEventLogDisplayLimitRaw();
 	FString GetEventLogFilterRaw();
+	bool ShouldHideNoiseEvents();
+	bool ShouldHideCollisionWindowEvents();
 }
 
 //==============================================================================
@@ -56,10 +56,27 @@ namespace SnapshotStoreConfig
 namespace StoreLifecycle
 {
 	UWorld* ResolveWorld(const UObject* InWorldContextObject);
-	void RemoveStoreForWorld(UWorld* InWorld);
 	DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore* FindStore(const UObject* InWorldContextObject);
 	DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore* FindOrAddStore(const UObject* InWorldContextObject);
+	void RemoveStoreForWorld(UWorld* InWorld);
 	void ResetAllStores();
+}
+
+//==============================================================================
+// Snapshot Record Builders
+//==============================================================================
+namespace SnapshotRecordBuilders
+{
+	DebugOverlaySnapshotStoreInternals::FDebugOverlaySnapshotStamp MakeSnapshotStamp(const UWorld* InWorld);
+	FString ToSafeEventName(const TCHAR* InEventName, const TCHAR* InFallback);
+	FString GetDisplayNameOrNA(const UObject* InObject);
+	FString NormalizeEnumDisplayText(const FString& InValue);
+	FString ToReasonOrNone(const TCHAR* InReason);
+	FString NormalizeReasonDisplayText(const FString& InValue);
+	FString ResolveCombatResultSourceName(const AActor* InResultReceiverActor, const FCombatResultPacket& InPacket);
+	bool IsSameCombatPair(const FDebugOverlayCombatSummary& InSummary, const FString& InSourceName, const FString& InTargetName);
+	FDebugOverlayEventEntry MakeEventEntry(const UWorld* InWorld, const FString& InCategory, const FString& InEventName, const FString& InOwnerName, const FString& InSourceName, const FString& InTargetName, const FString& InSummary);
+	void UpdateRecentCombatPair(DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore& InStore, const UWorld* InWorld, AActor* InSourceActor, AActor* InTargetActor, const FString& InEventName);
 }
 
 //==============================================================================
@@ -68,8 +85,8 @@ namespace StoreLifecycle
 namespace EventFilterPolicy
 {
 	FString NormalizeEventLogFilter(const FString& InFilter);
-	int32 GetClampedEventLogDisplayLimit();
 	FString GetCanonicalEventLogFilter();
+	int32 GetClampedEventLogDisplayLimit();
 	bool ShouldIncludeEventForDisplay(const FDebugOverlayEventEntry& InEntry, const FString& InFilter, bool bApplyDisplayFilters);
 	bool DoesEventMatchSubject(const FDebugOverlayEventEntry& InEntry, const FString& InSubjectName);
 	FDebugOverlayEventEntry MakeSubjectDisplayEventEntry(const FDebugOverlayEventEntry& InEntry, const FString& InSubjectName);
@@ -83,22 +100,5 @@ namespace EventRingAccess
 	void AddEventInternal(DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore& InStore, const FDebugOverlayEventEntry& InEntry);
 	TArray<FDebugOverlayEventEntry> GetRecentEventsCopyFromStore(const DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore& InStore, int32 InMaxEvents, int32 InMaxClamp, const FString& InFilter, bool bApplyDisplayFilters);
 	TArray<FDebugOverlayEventEntry> GetRecentEventsForSubjectCopyFromStore(const DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore& InStore, int32 InMaxEvents, int32 InMaxClamp, const FString& InFilter, const FString& InSubjectName, bool bApplyDisplayFilters);
-}
-
-//==============================================================================
-// Snapshot Record Builders
-//==============================================================================
-namespace SnapshotRecordBuilders
-{
-	DebugOverlaySnapshotStoreInternals::FDebugOverlaySnapshotStamp MakeSnapshotStamp(const UWorld* InWorld);
-	FString ToSafeEventName(const TCHAR* InEventName, const TCHAR* InFallback);
-	FString ToReasonOrNone(const TCHAR* InReason);
-	FString GetDisplayNameOrNA(const UObject* InObject);
-	FString NormalizeEnumDisplayText(const FString& InValue);
-	FString NormalizeReasonDisplayText(const FString& InValue);
-	FString ResolveCombatResultSourceName(const AActor* InResultReceiverActor, const FCombatResultPacket& InPacket);
-	bool IsSameCombatPair(const FDebugOverlayCombatSummary& InSummary, const FString& InSourceName, const FString& InTargetName);
-	FDebugOverlayEventEntry MakeEventEntry(const UWorld* InWorld, const FString& InCategory, const FString& InEventName, const FString& InOwnerName, const FString& InSourceName, const FString& InTargetName, const FString& InSummary);
-	void UpdateRecentCombatPair(DebugOverlaySnapshotStoreInternals::FDebugOverlayWorldStore& InStore, const UWorld* InWorld, AActor* InSourceActor, AActor* InTargetActor, const FString& InEventName);
 }
 #endif
